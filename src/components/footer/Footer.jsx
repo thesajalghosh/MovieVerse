7f96349de2ae4241a1fb93792cd1c8d2c11d5c1f
Sajal Ghosh <sajalghosh.itrd@gmail.com>
2023-03-25 17:03:01 +0530

    Added Footer Section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
 import "./Footer.css"
+import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin } from "react-icons/fa"
+import ContentWrapper from '../contentWrapper/ContentWrapper'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer className='footer'>
+      <ContentWrapper>
+        <ul className='footer__menuItems'>
+          <li className='footer__menuItem'>Terms Of Use</li>
+          <li className='footer__menuItem'>Privacy Policy</li>
+          <li className='footer__menuItem'>About</li>
+          <li className='footer__menuItem'>Blog</li>
+          <li className='footer__menuItem'>FAQ</li>
+        </ul>
+        <div className='infoText'>
+          A movie is an electronic signal of moving graphics, pictures, or text used to combine a steady stream of images used for entertainment, education, or other uses. This term describes content that's longer than ten minutes, or something you would watch on your TV or at the theater. Whereas video describes short clips or files on the Internet, see the video definition for further information and online example.
+
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebook />
+          </span>
+          <span className="icon">
+            <FaInstagram />
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,100 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Header.css'
+import { HiOutlineSearch } from 'react-icons/hi'
+import { SlMenu } from 'react-icons/sl'
+import { VscChromeClose } from 'react-icons/vsc'
+import { useNavigate, useLocation } from 'react-router-dom'
+
+import ContentWrapper from "../contentWrapper/ContentWrapper"
+import logo from "../../assets/movie-logo.png"
 
 const Header = () => {
+
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setquery] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() =>{
+    window.scrollTo(0,0);
+  }, [location])
+
+  const openSearch = () => {
+    setMobileMenu(false)
+    setShowSearch(true)
+  }
+
+  const openMobileMenu = () => {
+    setMobileMenu(true)
+    setShowSearch(false)
+  }
+  const searchQueryHandler = (event) => {
+
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`)
+
+      setTimeout(() => {
+        setShowSearch(false)
+      }, 1000);
+    }
+
+  }
+
+  const navigateHandlar = (type) =>{
+    if(type === "movie")
+    {
+      navigate("/explore/movie")
+    }
+    else{
+      navigate("/explore/tv")
+    }
+
+  }
+
+
   return (
-    <div>
-      Header
-    </div>
+    <header className='header'>
+      <ContentWrapper>
+        <div className='Header__container'>
+          <div className='logo'>
+            <img src={logo} alt='logo' />
+          </div>
+
+        
+             <div className='menuItems'>
+              <div className='menuItem'><HiOutlineSearch onClick={openSearch} /></div>
+                <div className='menuItem' onClick={() =>{navigateHandlar("movie")}}>Movies</div>
+                <div className='menuItem'onClick={() =>{navigateHandlar("tv")}}>TV Shows</div>
+             
+
+            </div>
+         
+        </div>
+        {/* <div className='mobileMenuItems'>
+        <HiOutlineSearch/>
+        {mobileMenu ? (<VscChromeClose onClick={() =>{
+          setMobileMenu(false)
+        }}/>) : ( <SlMenu onClick={openMobileMenu}/>)}
+       
+        
+        </div> */}
+      </ContentWrapper>
+      {showSearch && <div className='header__searchBar'>
+      <ContentWrapper>
+      <div className='header__search__input'>
+              <input type='text' placeholder='search for a movie or tv shows ...'
+                onChange={(e) => setquery(e.target.value)}
+                onKeyUp={searchQueryHandler}
+              />
+              <VscChromeClose className='close__input' onClick={() => setShowSearch(false)}/>
+            </div>
+      </ContentWrapper>
+
+      </div>}
+    </header>
   )
 }
 
